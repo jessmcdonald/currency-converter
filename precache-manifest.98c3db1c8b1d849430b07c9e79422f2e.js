64dfe9ec814ce66c62a6994529fd8bceaa56ecdc
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-04-15 18:22:15 +0200

    Updates

---
+++ precache-manifest.98c3db1c8b1d849430b07c9e79422f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896fd70976748fe9b2123e05182debea",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "96d036a8ff218c2d23c0",
+    "url": "/currency-converter/static/css/main.aea05aa4.chunk.css"
+  },
+  {
+    "revision": "8a3b5abf91db1cffc2b1",
+    "url": "/currency-converter/static/js/2.358afee7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.358afee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d036a8ff218c2d23c0",
+    "url": "/currency-converter/static/js/main.c8094c85.chunk.js"
+  },
+  {
+    "revision": "1d6ffa0696764d79a73a",
+    "url": "/currency-converter/static/js/runtime-main.f4cdd565.js"
+  }
+]);
